27466c250f6021a65a4daadd1c22e36a42235b13
JHAZ <julien.hazebroucq@gmail.com>
2021-12-03 13:10:44 +0100

    step1

--- MagicMirror-Module-Snmp.js
+++ MagicMirror-Module-Snmp.js
@@ -16,6 +16,7 @@ Module.register("{{MODULE_NAME}}", {
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
 	start: function() {
+		console.log ("ici");
 		var self = this;
 		var dataRequest = null;
 		var dataNotification = null;
@@ -37,8 +38,38 @@ Module.register("{{MODULE_NAME}}", {
 	 *
 	 */
 	getData: function() {
+		
 		var self = this;
 
+		var snmp = require ("net-snmp");
+
+		var session = snmp.createSession ("192.168.1.10", "perso");
+		var oids = ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.1.6.0"];
+
+
+		session.get (oids, function (error, varbinds) {
+			if (error) {
+				console.error (error);
+			} else {
+				for (var i = 0; i < varbinds.length; i++) {
+					if (snmp.isVarbindError (varbinds[i])) {
+						console.error (snmp.varbindError (varbinds[i]));
+					} else {
+						console.log (varbinds[i].oid + " = " + varbinds[i].value);
+					}
+				}
+			}
+			session.close ();
+		});
+
+		
+		
+		session.trap (snmp.TrapType.LinkDown, function (error) {
+			if (error) {
+				console.error (error);
+			}
+		});
+		
 		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
 		var retry = true;
 
